d811c97a1bd321fbbf020246926aa9fd028c7674
aibotsoft <aibotsoft@gmail.com>
2023-01-25 16:54:54 +0200

    add postgres

---
+++ postgres/go.mod
@@ -0,0 +1,24 @@
+module github.com/aibotsoft/pkg/postgres
+
+go 1.19
+
+require (
+	github.com/exaring/otelpgx v0.3.1
+	github.com/jackc/pgx-shopspring-decimal v0.0.0-20220624020537-1d36b5a1853e
+	github.com/jackc/pgx/v5 v5.2.0
+)
+
+require (
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/puddle/v2 v2.1.2 // indirect
+	github.com/shopspring/decimal v1.3.1 // indirect
+	go.opentelemetry.io/otel v1.11.1 // indirect
+	go.opentelemetry.io/otel/trace v1.11.1 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	golang.org/x/crypto v0.1.0 // indirect
+	golang.org/x/sync v0.0.0-20220923202941-7f9b1623fab7 // indirect
+	golang.org/x/text v0.4.0 // indirect
+)
--- postgres/postgres.go
+++ postgres/postgres.go
@@ -3,33 +3,37 @@ package postgres
 import (
 	"context"
 	"fmt"
-	"github.com/aibotsoft/livebot/pkg/config"
 	"github.com/exaring/otelpgx"
 	pgxdecimal "github.com/jackc/pgx-shopspring-decimal"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"time"
 )
 
-func NewMustPostgresPool(cfg *config.Config) *pgxpool.Pool {
+type Config struct {
+	URL     string
+	Timeout time.Duration
+}
+
+func NewMustPostgresPool(cfg *Config) *pgxpool.Pool {
 	conn, err := NewPostgresPool(cfg)
 	if err != nil {
 		panic(err)
 	}
 	return conn
 }
-func NewPostgresPool(cfg *config.Config) (*pgxpool.Pool, error) {
-	connConfig, err := pgxpool.ParseConfig(cfg.Postgres.URL)
+func NewPostgresPool(cfg *Config) (*pgxpool.Pool, error) {
+	connConfig, err := pgxpool.ParseConfig(cfg.URL)
 	if err != nil {
 		return nil, fmt.Errorf("parse postgres url config error: %w", err)
 	}
-	//fmt.Printf("%+v", connConfig)
 	connConfig.AfterConnect = func(ctx context.Context, conn *pgx.Conn) error {
 		pgxdecimal.Register(conn.TypeMap())
 		return nil
 	}
 	connConfig.ConnConfig.Tracer = otelpgx.NewTracer()
 
-	ctx, cancel := context.WithTimeout(context.Background(), cfg.Postgres.Timeout)
+	ctx, cancel := context.WithTimeout(context.Background(), cfg.Timeout)
 	defer cancel()
 	db, err := pgxpool.NewWithConfig(ctx, connConfig)
 	if err != nil {
@@ -38,13 +42,13 @@ func NewPostgresPool(cfg *config.Config) (*pgxpool.Pool, error) {
 	return db, nil
 }
 
-func NewPostgres(cfg *config.Config) (*pgx.Conn, error) {
-	connConfig, err := pgx.ParseConfig(cfg.Postgres.URL)
+func NewPostgres(cfg *Config) (*pgx.Conn, error) {
+	connConfig, err := pgx.ParseConfig(cfg.URL)
 	if err != nil {
 		return nil, fmt.Errorf("parse postgres url config error: %w", err)
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), cfg.Postgres.Timeout)
+	ctx, cancel := context.WithTimeout(context.Background(), cfg.Timeout)
 	defer cancel()
 	db, err := pgx.ConnectConfig(ctx, connConfig)
 	if err != nil {
@@ -52,7 +56,7 @@ func NewPostgres(cfg *config.Config) (*pgx.Conn, error) {
 	}
 	return db, nil
 }
-func NewMustPostgres(cfg *config.Config) *pgx.Conn {
+func NewMustPostgres(cfg *Config) *pgx.Conn {
 	conn, err := NewPostgres(cfg)
 	if err != nil {
 		panic(err)
